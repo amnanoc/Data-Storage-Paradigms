fa5ff4509673117dbd82af5503079c0135bbcab4
Amna <129167927+amnanoc@users.noreply.github.com>
2023-12-04 03:37:00 +0100

    Update HistoricalData.sql

--- HistoricalData.sql
+++ HistoricalData.sql
@@ -1,32 +1,36 @@
 -- Create historical database tables
 CREATE TABLE HistoricalLesson (
-    LessonID SERIAL PRIMARY KEY,
-    LessonType CHAR(20) NOT NULL,
+    LessonID SERIAL NOT NULL,
+    LessonType LessonTypes NOT NULL,
     Genre CHAR(10),
     Instrument CHAR(50),
     LessonPrice DECIMAL(10) NOT NULL,
     StudentName CHAR(50) NOT NULL,
-    StudentEmail CHAR(100) NOT NULL
+    StudentEmail CHAR(100) NOT NULL,
+    LessonDate DATE NOT NULL,
+    PRIMARY KEY (LessonID)
 );
 
--- Copy data from present database to historical database
-INSERT INTO HistoricalLesson (LessonType, Genre, Instrument, LessonPrice, StudentName, StudentEmail)
+-- Copy Lesson data to HistoricalLesson
+INSERT INTO HistoricalLesson (LessonID, LessonType, Genre, Instrument, LessonPrice, StudentName, StudentEmail, LessonDate)
 SELECT
+    l.LessonID,
     l.LessonType,
-    CASE WHEN l.LessonType = 'ensemble' THEN l.TargetGenre ELSE NULL END,
-    CASE WHEN l.LessonType != 'ensemble' THEN t.Name ELSE NULL END,
-    ps.StudentPaying,
-    s.Name,
-    s.Email
+    l.TargetGenre,
+    i.Name AS Instrument,
+    ps.StudentPaying AS LessonPrice,
+    s.Name AS StudentName,
+    s.Email AS StudentEmail,
+    CURRENT_DATE AS LessonDate
 FROM
     Lesson l
-LEFT JOIN
+JOIN
     LessonInstruments li ON l.LessonID = li.LessonID
-LEFT JOIN
-    Teaching t ON li.InstrumentID = t.InstrumentID
-LEFT JOIN
-    PricingScheme ps ON l.PricingSchemeID = ps.PricingSchemeID
-LEFT JOIN
+JOIN
+    Instrument i ON li.InstrumentID = i.InstrumentID
+JOIN
     LessonAttendance la ON l.LessonID = la.LessonID
-LEFT JOIN
-    Student s ON la.StudentID = s.StudentID;
+JOIN
+    Student s ON la.StudentID = s.StudentID
+JOIN
+    PricingScheme ps ON l.PricingSchemeID = ps.PricingSchemeID;
